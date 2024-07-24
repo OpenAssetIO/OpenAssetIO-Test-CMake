0ada3e14923f347c324336fc46f61d3151898c65
David Feltell <david.feltell@foundry.com>
2024-07-24 12:04:02 +0100

    [Tests] Use non-deprecated TraitsData for MediaCreation test
    Part of OpenAssetIO/OpenAssetIO#1311. In #13 we missed an `#include`
    that was still using the deprecated location of the `TraitsData` type.
    
    Signed-off-by: David Feltell <david.feltell@foundry.com>

--- src/test.mediacreation.cpp
+++ src/test.mediacreation.cpp
@@ -3,13 +3,13 @@
 
 // Include all headers to test they are where we expect and can be
 // compiled.
-#include <openassetio/TraitsData.hpp>
+#include <openassetio/trait/TraitsData.hpp>
 
 #include <openassetio_mediacreation/openassetio_mediacreation.hpp>
 
 int main() {
   auto trait = openassetio_mediacreation::traits::managementPolicy::ManagedTrait(
-      openassetio::TraitsData::make());
+      openassetio::trait::TraitsData::make());
 
   trait.imbue();
   return 0;
